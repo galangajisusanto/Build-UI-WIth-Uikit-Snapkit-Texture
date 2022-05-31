47aac068cecfd0e6a10c01549c3dde3b4fdc8b53
Galang Aji Susanto <galang.aji@mekari.com>
2022-05-31 22:42:00 +0700

    Initial Commit

---
+++ Build UI WIth Snapkit Texture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Build UI WIth Snapkit Texture
+//
+//  Created by MEKARI on 31/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
